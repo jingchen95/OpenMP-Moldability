458ad97c6e4981541744cda7a2a078a5c7d3e134
Olle Nilsson <olleni@student.chalmers.se>
2022-04-12 10:56:50 +0200

    added makefile

--- openmp/tests/gather_data.py
+++ openmp/tests/gather_data.py
@@ -1,8 +1,8 @@
-
+import os
 import subprocess
 
 matmul_input = [32, 64, 128, 256]
-memory_input = [20000, 200000]
+memory_input = [10000, 20000, 100000, 200000]
 taskloop_input = [100000, 1000000, 10000000]
 jacobi_input = [100, 1000]
 
@@ -16,12 +16,15 @@ def run_program(app, iter):
     print("Started benchmark {test}".format(test=app[0].strip("./")))
     for input in app[1]:
         for i in range(iter):
-            subprocess.run(args=[app[0], "1000", str(input)], shell=True,  text=False,
+            subprocess.run(args=[app[0], "1000", str(input)], shell=True,
+                           text=False,
                            capture_output=False, stdout=subprocess.DEVNULL)
 
     print("Done benchmarking {test}".format(test=app[0].strip("./")))
 
 
 if __name__ == '__main__':
+    os.environ['OMP_PLACES'] = 'threads'
+    os.environ['OMP_PROC_BIND'] = 'true'
     for test in tests:
         run_program(test, 10)
